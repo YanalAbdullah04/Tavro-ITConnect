7971b55d4f8851f869c4537409ffecc8dee69ba5
YanalAbdullah04 <Yanalabdullh@gmail.com>
2026-06-20 02:08:35 +0300

    adding gettrainee and fire trainee endpoints

---
+++ ITConnect/Data/ResponsesModel/CompanyTraineeResponse.cs
@@ -0,0 +1,10 @@
+namespace ITConnect.Data.ResponsesModel
+{
+    public class CompanyTraineeResponse
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string TrainingSessionName { get; set; }
+    }
+}
--- ITConnect/Services/CompanyService.cs
+++ ITConnect/Services/CompanyService.cs
@@ -1,6 +1,7 @@
 using ITConnect.Data.ResponsesModel;
 using ITConnect.Iservices;
 using ITConnect.Models.Repositories;
+using ITConnect.Services.Iservices;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -9,10 +10,12 @@ namespace ITConnect.Services
     public class CompanyService : ICompanyService
     {
         private readonly ICompanyRepository companyRepository;
+        private readonly IUserContext userContext;
 
-        public CompanyService(ICompanyRepository companyRepository)
+        public CompanyService(ICompanyRepository companyRepository, IUserContext userContext)
         {
             this.companyRepository = companyRepository;
+            this.userContext = userContext;
         }
 
         public async Task<List<CompanyResponse>> GetCompanyResponseAsync()
@@ -23,6 +26,24 @@ namespace ITConnect.Services
         public async Task<List<CompanyTrackResponse>> GetCompanyTracksAsync(string companyId)
         {
             return await companyRepository.GetCompanyTracksAsync(companyId);
+        }
+
+        public async Task<List<CompanyTraineeResponse>> GetCompanyTraineesAsync(string? searchQuery)
+        {
+            var companyId = userContext.CompanyId;
+            if (string.IsNullOrEmpty(companyId))
+                return new List<CompanyTraineeResponse>();
+
+            return await companyRepository.GetCompanyTraineesAsync(companyId, searchQuery);
+        }
+
+        public async Task<bool> DeleteCompanyTraineeAsync(string traineeId)
+        {
+            var companyId = userContext.CompanyId;
+            if (string.IsNullOrEmpty(companyId))
+                return false;
+
+            return await companyRepository.DeleteCompanyTraineeAsync(companyId, traineeId);
         }
     }
 }
